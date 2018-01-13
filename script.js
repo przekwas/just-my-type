b79369796028fce055a3128022e85dd792f5719d
Luke Przekwas <galactica@Lukes-MacBook-Pro.local>
2018-01-12 18:46:18 -0600

    logic for correctkeypress and incorrectkeypress

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-
-
-/*THIS IS THE FUNCTIONS SECTION*/
-
-function highlightButton(id) {
-    let idNumber = $("#" + id);
-    idNumber.css({ "background-color": "rgb(255,255,0)" });
-}
-
-function nohighlightButton(id) {
-    let idNumber = $("#" + id);
-    idNumber.css({ "background-color": "#f5f5f5" });
-}
-
-
-
-/*THIS ENDS THE FUNCTIONS SECTION*/
-
-
-
+let sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 
 //on page load, hide the uppercase
 $(document).ready(function () {
     $("#keyboard-upper-container").toggle();
+
+    //making my life easier
+    let body = $("body");
 
     //this will check for someone pressing shift, if held, show the uppercase
     $(document).keydown(function (event) {
@@ -30,7 +14,7 @@ $(document).ready(function () {
             $("#keyboard-upper-container").toggle();
         } 
     })
-    //this will check for someone letting go of shift, if let go, show the lowercase again
+    //this will check for someone letting go of shift, if let go, show the lowercase again AND it'll remove the yellow bgcolor class
     $(document).keyup(function (event) {
         if (event.which === 16 || event.which === 20) {
             $("#keyboard-upper-container").hide();
@@ -39,10 +23,18 @@ $(document).ready(function () {
         $('.highlight').removeClass('highlight');
     })
 
+    $("#sentence").append(sentences[0]);
+
+    //will match up a keypress id and add a class to change its bgcolor
     $(document).keypress(function (event) {
         let keyPress = event.which;
         $('#' + keyPress).addClass('highlight');
-    })
-    
 
+        //logic to check if current keypress matches with the leading letter
+        if (sentences[0].charCodeAt(0) === keyPress){
+            console.log("YOU RIGHT");
+        } else if (sentences[0].charCodeAt(0) !== keyPress) {
+            console.log("YOU WRONG");
+        }
+    })
 })
