0f7964e55c654e9cb1ec5f9f40e043dd028d262d
Luke Przekwas <galactica@Lukes-MacBook-Pro.local>
2018-01-13 11:18:59 -0600

    yellow block moving on keypress

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
         if (event.which === 16 || event.which === 20) {
             $("#keyboard-lower-container").hide();
             $("#keyboard-upper-container").toggle();
-        } 
+        }
     })
     //this will check for someone letting go of shift, if let go, show the lowercase again AND it'll remove the yellow bgcolor class
     $(document).keyup(function (event) {
@@ -20,21 +20,39 @@ $(document).ready(function () {
             $("#keyboard-upper-container").hide();
             $("#keyboard-lower-container").toggle();
         }
+        //changes bg color back to normal
         $('.highlight').removeClass('highlight');
     })
 
+    let numberOfMistakes = 0;
+    let numberCorrect = 0;
     $("#sentence").append(sentences[0]);
+
+    //gets the lengths of each sentence in the array
+    let lengths = sentences.map(function (word) {
+        return word.length
+    });
+
+
 
     //will match up a keypress id and add a class to change its bgcolor
     $(document).keypress(function (event) {
         let keyPress = event.which;
         $('#' + keyPress).addClass('highlight');
-
-        //logic to check if current keypress matches with the leading letter
-        if (sentences[0].charCodeAt(0) === keyPress){
-            console.log("YOU RIGHT");
-        } else if (sentences[0].charCodeAt(0) !== keyPress) {
-            console.log("YOU WRONG");
+        if (event.which > 46 && event.which !== 91 || event.which === 32) {
+            $("#yellow-block").animate({ left: "+=17" }, { duration: 1, easing: "linear" });
+            console.log(event.which);
         }
+
+        // for (let i = 0; i < lengths.length; i++) {
+        //     //logic to check if current keypress matches with the leading letter
+        //     if (lengths[i].charCodeAt(i) === keyPress) {
+        //         numberCorrect++;
+        //         console.log("YOU RIGHT");
+        //     } else if (lengths[i].charCodeAt(i) !== keyPress) {
+        //         numberOfMistakes++;
+        //         console.log("YOU WRONG");
+        //     }
+        // }
     })
 })
